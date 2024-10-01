6d8feed6d5fc1464104be08f98962acb1d3b8f99
Lautaro <lau-yeah@hotmail.com>
2024-10-01 20:10:42 -0300

    Se arregló ruta de img y se actualizo la base de datos

--- src/pages/components/cards/CardBase.js
+++ src/pages/components/cards/CardBase.js
@@ -1,21 +1,16 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import image from '../../../../public/img/eloquent-js.jpeg';
-import BuyButton from '../button/PrimaryButton';
- 
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
+import BuyButton from "../button/PrimaryButton";
 
 function CardBase() {
   return (
-    <Card className='card-container' style={{ width: '18rem' }}>
-      <Card.Img variant="top" src='./Logo.jpeg' />
+    <Card className="card-container" style={{ width: "18rem" }}>
+      <Card.Img variant="top" src="./img/deja-de-ser-tu.jpeg" />
       <Card.Body>
         <Card.Title>Nombre del libro</Card.Title>
-        <Card.Text>
-        Descripcion del articulo
-        </Card.Text>
-        <Card.Text>
-          Precio: 
-        </Card.Text>
+        <Card.Text>Descripcion del articulo</Card.Text>
+        <Card.Text>Precio:</Card.Text>
         <BuyButton />
       </Card.Body>
     </Card>
