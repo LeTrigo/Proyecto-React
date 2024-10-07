1aef44d04eae3c090a4dff17938fbbfae4cf862e
Javier Vallejos <160947104+Shironeko85@users.noreply.github.com>
2024-10-07 15:00:12 -0300

    Saque el boton para el carrito

--- src/pages/components/cards/CardBase.js
+++ src/pages/components/cards/CardBase.js
@@ -1,19 +1,19 @@
 import Card from "react-bootstrap/Card";
-import BuyButton from "../button/PrimaryButton";
+import BuyButton from "../button/BuyButton";
 
 function CardBase(props) {
 
-  const {image, name, description, price} = props.books
+/*   const {image, name, description, price} = props.books */
 
 
 
   return (
     <Card className="card-container" style={{ width: "18rem" }}>
-      <Card.Img variant="top" src={image} />
+      <Card.Img variant="top" src="./img/eloquent-js.jpeg" />
       <Card.Body>
-        <Card.Title>{name}</Card.Title>
-        <Card.Text>{description}</Card.Text>
-        <Card.Text>`Precio: ${price}`</Card.Text>
+        <Card.Title>name</Card.Title>
+        <Card.Text>description</Card.Text>
+        <Card.Text>`Precio: $price`</Card.Text>
         <BuyButton />
       </Card.Body>
     </Card>
