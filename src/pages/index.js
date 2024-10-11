51f401857fee55bfc522121fec85fe4341785649
Agustin Arroyo <agusarroyolaurito@gmail.com>
2024-10-11 15:54:59 -0300

    Armando la pagina, falta hacer funcionar el carrito, y corregir la navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,21 @@
+import CardContainer from "./components/cards/CardContainer";
+import Carrousel from "./components/carousel/Carrousel";
+import Footer from "./components/footer/Footer";
 import InfoCard from "./components/InfoCard";
+import NavBar from "./components/NavBar/NavBar";
 import ProposelCard from "./components/ProposelCard";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 
 export default function Home() {
   return (
     <>
+      <NavBar />
       <ProposelCard />
+      <CardContainer />
       <InfoCard />
+      <Carrousel />
+      <Footer />
     </>
 
   );
