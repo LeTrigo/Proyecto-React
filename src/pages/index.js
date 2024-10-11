aaa3a4d15fa6a9c17e22b1524de7bea898e5ebc5
Agustin Arroyo <agusarroyolaurito@gmail.com>
2024-10-11 00:09:32 -0300

    fusionando las ramas al proyecto

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar/NavBar';
+
+
+export default function Home() {
+  return (
+    <main>
+      <NavBar />
+    </main>
+  );
+}
