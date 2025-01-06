c2152422f61e637aa1ada7545e16e37625de8c64
Lautaro <lau-yeah@hotmail.com>
2025-01-06 18:03:23 -0300

    Eliminado cart sin usar

--- src/pages/components/Cart.js
+++ src/pages/components/Cart.js
@@ -1,20 +1,15 @@
-import {  useContext } from "react";
+import { useContext } from "react";
 import { CartContext } from "@/context/cartContext";
 import CardContainer from "./cards/CardContainer";
 
-
 const Cart = () => {
-  const {
-    state,
-  } = useContext(CartContext);
+  const { state } = useContext(CartContext);
 
-  const { products, cart } = state;
+  const { products } = state;
 
   return (
     <>
-     
-      <CardContainer books={products}/>
-    
+      <CardContainer books={products} />
     </>
   );
 };
