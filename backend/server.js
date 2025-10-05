097fb135338f093d208c51a3a911ef33225002d6
Lautaro <lau-yeah@hotmail.com>
2025-10-05 11:53:20 -0300

    Deploy prueba 4

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 // server.js
 const jsonServer = require("json-server");
+const path = require("path");
 const server = jsonServer.create();
-const router = jsonServer.router("src/db/db.json");
+const router = jsonServer.router(path.join(__dirname, "../src/db/db.json"));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
