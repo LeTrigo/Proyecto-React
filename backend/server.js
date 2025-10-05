a1ea005fbb647164f91a03a4c4b3bb13331dd6ef
Lautaro <lau-yeah@hotmail.com>
2025-10-05 11:59:58 -0300

    Deploy prueba 5

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,27 @@
 const jsonServer = require("json-server");
 const path = require("path");
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "../src/db/db.json"));
-const middlewares = jsonServer.defaults();
+const router = jsonServer.router(path.join(__dirname, "db.json"));
+const middlewares = jsonServer.defaults({
+  static: path.join(__dirname, "public"),
+});
 
 server.use(middlewares);
+
+// Enable CORS for all routes
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
+
 server.use(router);
 
 const port = process.env.PORT || 5000;
